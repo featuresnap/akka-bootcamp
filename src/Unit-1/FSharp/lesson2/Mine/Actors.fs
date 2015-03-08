204969261348c12ea0f2f3950fb6cdd611a95729
Patrick Kelly <pkelly14159@hotmail.com>
2015-03-08 19:23:10 -0400

    Printing of instructions is now performed by consoleReaderActor in response to StartCommand

--- src/Unit-1/FSharp/lesson2/Mine/Actors.fs
+++ src/Unit-1/FSharp/lesson2/Mine/Actors.fs
@@ -3,6 +3,8 @@
 open System
 open Akka.Actor
 open Akka.FSharp
+open Messages
+open Utility
 
 [<Literal>]
 let ExitCommand = "exit"
@@ -10,13 +12,19 @@ let ExitCommand = "exit"
 [<Literal>]
 let ContinueCommand = "continue"
 
-let consoleReaderActor (consoleWriter: ActorRef) (mailbox: Actor<_>) message = 
-    let line = Console.ReadLine ()
-    match line.ToLower () with
-    | ExitCommand -> mailbox.Context.System.Shutdown ()
-    | _ -> 
-        consoleWriter <! line
-        mailbox.Self  <! ContinueCommand
+let consoleReaderActor (consoleWriter: ActorRef) (mailbox: Actor<_>) (message:obj) = 
+    match message with 
+    | :? StartCommand -> 
+        do printInstructions() |> ignore
+        mailbox.Self <! ContinueCommand
+    |_ ->
+        
+        let line = Console.ReadLine ()
+        match line.ToLower () with
+        | ExitCommand -> mailbox.Context.System.Shutdown ()
+        | _ -> 
+            consoleWriter <! line
+            mailbox.Self  <! ContinueCommand
 
 let consoleWriterActor message = 
     let (|Even|Odd|) n = if n % 2 = 0 then Even else Odd
