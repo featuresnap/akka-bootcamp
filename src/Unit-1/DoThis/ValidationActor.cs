ed72998fd7d57521a630413fd96fc3e9366587c0
Patrick Kelly <patrick.kelly@sherwin.com>
2015-05-12 21:16:12 -0400

    Added test project. Began test driving ValidationActor.

---
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Akka.Actor;
+
+namespace WinTail
+{
+    public class ValidationActor:UntypedActor
+    {
+        private IActorRef _consoleWriter;
+
+        public ValidationActor(IActorRef consoleWriter)
+        {
+            _consoleWriter = consoleWriter;
+        }
+
+        protected override void OnReceive(object message)
+        {
+            _consoleWriter.Tell(new Messages.NullInputError("Please input at least one character."));
+            
+        }
+    }
+}
---
+++ src/Unit-1/DoThis/WinTail.Tests/ValidationActorTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.TestKit.NUnit;
+using NUnit.Framework;
+
+namespace WinTail.Tests
+{
+    [TestFixture]
+    public class ValidationActorTests:TestKit
+    {
+        [Test]
+        public void CanCreateValidationActor()
+        {
+            var actor = Sys.ActorOf(Props.Create(() => new ValidationActor(TestActor)));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void EmptyInput_Sends_NullInputError_To_ConsoleWriterActor(string input)
+        {
+            var consoleWriter = TestActor;
+            var actor = Sys.ActorOf(Props.Create<ValidationActor>(consoleWriter));
+            actor.Tell(input);
+            ExpectMsg<Messages.NullInputError>();
+        }
+
+        
+       
+
+    }
+}
