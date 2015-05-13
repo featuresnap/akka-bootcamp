2ea373fda0fc1063ec1d4d2eadd6463f8f9137f1
Patrick Kelly <patrick.kelly@sherwin.com>
2015-05-13 13:06:01 -0400

    Tests passing individually, some coupling prevents tests from succeeding together

--- src/Unit-1/DoThis/ValidationActor.cs
+++ src/Unit-1/DoThis/ValidationActor.cs
@@ -14,8 +14,33 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            _consoleWriter.Tell(new Messages.NullInputError("Please input at least one character."));
-            
+            var msg = message as string;
+
+            if (string.IsNullOrEmpty(msg))
+            {
+                _consoleWriter.Tell(new Messages.NullInputError("Please input at least one character."));
+            }
+            else
+            {
+                var valid = IsValidInput(msg);
+
+                if (valid)
+                {
+                    _consoleWriter.Tell(new Messages.InputSuccess("Thank you. Input was valid."));
+                    
+                }
+                else
+                {
+                    _consoleWriter.Tell(new Messages.ValidationError("Input was invalid. Must contain even number of characters."));
+                }
+            }
+
+            Sender.Tell(new Messages.ContinueProcessing());
+        }
+
+        private bool IsValidInput(string message)
+        {
+            return message.Length % 2 == 0;
         }
     }
 }
--- src/Unit-1/DoThis/WinTail.Tests/ValidationActorTests.cs
+++ src/Unit-1/DoThis/WinTail.Tests/ValidationActorTests.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Akka.Actor;
+using Akka.TestKit;
 using Akka.TestKit.NUnit;
 using NUnit.Framework;
 
@@ -11,24 +13,55 @@ namespace WinTail.Tests
     [TestFixture]
     public class ValidationActorTests:TestKit
     {
-        [Test]
-        public void CanCreateValidationActor()
+        private IActorRef _consoleWriter;
+        private IActorRef _validationActor;
+
+        [SetUp]
+        public void SetUp()
         {
-            var actor = Sys.ActorOf(Props.Create(() => new ValidationActor(TestActor)));
+            _consoleWriter = TestActor;
+            _validationActor = Sys.ActorOf(Props.Create<ValidationActor>(_consoleWriter));
         }
+
 
         [TestCase(null)]
         [TestCase("")]
         public void EmptyInput_Sends_NullInputError_To_ConsoleWriterActor(string input)
         {
-            var consoleWriter = TestActor;
-            var actor = Sys.ActorOf(Props.Create<ValidationActor>(consoleWriter));
-            actor.Tell(input);
+            _validationActor.Tell(input);
+         
             ExpectMsg<Messages.NullInputError>();
         }
 
-        
-       
+        [Test]
+        public void ValidInput_Sends_InputSuccess_To_ConsoleWriterActor()
+        {
+            var validInput = "abcd";
+            _validationActor.Tell(validInput);
+            ExpectMsg<Messages.InputSuccess>();
+        }
+
+        [Test]
+        public void InvalidInput_Sends_ValidationError_To_ConsoleWriterActor()
+        {
+            var invalidInput = "x";
+            _validationActor.Tell(invalidInput);
+            ExpectMsg<Messages.ValidationError>();
+        }
+
+        [Test]
+        public async Task  ValidationActor_Tells_Sender_ContinueProcessing()
+        {
+            var input = "f";
+
+            var senderResponse =  await _validationActor.Ask("Foo", TimeSpan.FromSeconds(3.0));
+
+            Assert.That(senderResponse, Is.InstanceOf<Messages.ContinueProcessing>());
+        }
+
+
+
+
 
     }
 }
