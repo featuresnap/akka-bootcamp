9ecb0ba34a7bf291b5a521b58ae6f8a229c846ec
Patrick Kelly <pkelly14159@hotmail.com>
2015-05-31 06:52:13 -0400

    Completed unit 2

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
 using Akka.Actor;
 
 namespace ChartApp.Actors
 {
-    public class ChartingActor : ReceiveActor
+    public class ChartingActor : ReceiveActor, IWithUnboundedStash
     {
         #region Messages
 
@@ -40,26 +41,70 @@ namespace ChartApp.Actors
             }
         }
 
-       #endregion
+        public class TogglePause
+        {
+        }
+
+        #endregion
 
         private readonly Chart _chart;
         private Dictionary<string, Series> _seriesIndex;
         private const int MAX_NUMBER_OF_POINTS = 250;
         private int _xPosCounter = 0;
+        private Button _pauseButton;
 
-        public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>())
+        public ChartingActor(Chart chart, Button pauseButton)
+            : this(chart, new Dictionary<string, Series>(), pauseButton)
         {
         }
 
-        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
+        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex, Button pauseButton)
         {
+            _pauseButton = pauseButton;
             _chart = chart;
             _seriesIndex = seriesIndex;
+            Charting();
 
+        }
+
+        private void Charting()
+        {
             Receive<InitializeChart>(initMsg => HandleInitialize(initMsg));
             Receive<AddSeries>(addCmd => HandleAddSeries(addCmd));
             Receive<RemoveSeries>(removeCmd => HandleRemoveSeries(removeCmd));
             Receive<Metric>(metric => HandleMetrics(metric));
+
+            Receive<TogglePause>(toggle =>
+            {
+                SetPauseButtonText(true);
+                BecomeStacked(Paused);
+            });
+        }
+
+        private void Paused()
+        {
+            Receive<AddSeries>(_ => Stash.Stash());
+            Receive<RemoveSeries>(_ => Stash.Stash());
+            Receive<Metric>(metric => HandleMetricsPaused(metric));
+            Receive<TogglePause>(toggle =>
+             {
+                 SetPauseButtonText(false);
+                 UnbecomeStacked();
+                 Stash.UnstashAll();
+             });
+        }
+
+        private void SetPauseButtonText(bool paused)
+        {
+            if (paused)
+            {
+                _pauseButton.Text = "RUN >>";
+            }
+            else
+            {
+                _pauseButton.Text = "PAUSE II";
+            }
+
         }
 
         #region Individual Message Type Handlers
@@ -117,12 +162,27 @@ namespace ChartApp.Actors
         }
 
 
+
         private void HandleAddSeries(AddSeries series)
         {
             if (!string.IsNullOrEmpty(series.Series.Name) && !_seriesIndex.ContainsKey(series.Series.Name))
             {
                 _seriesIndex.Add(series.Series.Name, series.Series);
                 _chart.Series.Add(series.Series);
+                SetChartBoundaries();
+            }
+        }
+
+        private void HandleMetricsPaused(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) && _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(_xPosCounter++, 0.0d);
+                while (series.Points.Count > MAX_NUMBER_OF_POINTS)
+                {
+                    series.Points.RemoveAt(0);
+                }
                 SetChartBoundaries();
             }
         }
@@ -134,7 +194,7 @@ namespace ChartApp.Actors
             double maxAxisX, maxAxisY, minAxisX, minAxisY = 0.0d;
 
             var allPoints = new HashSet<DataPoint>(
-                _seriesIndex.Values.SelectMany(series=>series.Points));
+                _seriesIndex.Values.SelectMany(series => series.Points));
 
             var yValues = allPoints.SelectMany(point => point.YValues).ToList();
 
@@ -153,5 +213,6 @@ namespace ChartApp.Actors
             }
         }
 
+        public IStash Stash { get; set; }
     }
 }
